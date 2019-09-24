bed18affb10bf5cadaeb60b6235b15892ffd5e2d
Robert Hajek <robert.hajek@gmail.com>
2019-09-24 19:07:36 +0200

    added docs templates

--- docs/conf.py
+++ docs/conf.py
@@ -56,7 +56,7 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.todo',
     'sphinx.ext.viewcode',
-    'sphinx_autodoc_typehints'
+    # 'sphinx_autodoc_typehints'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
