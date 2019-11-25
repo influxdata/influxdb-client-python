cb579de02063d943db282a9b9a713bbdcf82702c
Anaisdg <dganais@gmail.com>
2019-11-25 16:32:38 -0600

    test

--- influxdb_client/client/flux_builder.py
+++ influxdb_client/client/flux_builder.py
@@ -1,5 +1,9 @@
 class qBuilder():
     def __init__(self):
+        self.bucket_name = None
+        self.start_time = None
+        self.stop_time = None
+        self.filter_tuples = None
         self.build_bucket = ""
         self.build_time = ""
         self.flatten_param = ""
@@ -7,46 +11,52 @@ class qBuilder():
         self.build_flatten = ""
         self.build_flux_query = ""
 
-    def bucket(self,bucket):
-        self.build_bucket = f'from(bucket: "{bucket}")'
+    def bucket(self, bucket):
+        self.bucket_name = bucket
+        self.build_bucket = f'from(bucket: bucket_name)'
         return self
 
     def range(self, start, stop):
-        self.build_time = f'|> range(start:{start}, stop:{stop})'
-        return self 
+        self.start_time = start
+        self.stop_time = stop
+        self.build_time = f'|> range(start: start_time, stop: stop_time)'
+        return self
 
-    def build_equals(self, key, value, equality = "==", prefix="r",):
+    def build_equals(self, key, value, equality="==", prefix="r",):
         if type(value) == str:
-            return f'{prefix}.{key} == "{value}"' 
+            return f'{prefix}.{key} == "{value}"'
         if type(value) == int:
             return f'{prefix}.{key} {equality} {value}'
 
-    def filters(self, filters, equality = "=="):
+    def filters(self, filters, equality="=="):
+        # ("_measurement", ["abc", "def"]), ("key", ["value"])
+        # q2 = qBuilder().filters(filters=[("_value", [1])], equality = ">"
+        # filters=[("_measurement", ["abc"])])
+        # self.measurements = measurements
+        # self.tags = tags
+        # self.fields = fields
+        self.filter_tuples = filters
         filters_queries = []
 
         for filter in filters:
-            or_query = ' or '.join([self.build_equals(filter[0], value, equality) for value in filter[1]])
+            or_query = ' or '.join(
+                [self.build_equals(filter[0], value, equality) for value in filter[1]])
             filters_queries.append(f'|> filter(fn: (r) => {or_query})')
-            self.build_filters = "\n".join(filters_queries)
+            self.build_filters = "".join(filters_queries)
         return self
 
-    def flatten(self, flat=True):
-        if flat == True:
-            self.build_flatten = f'|> group()'
-            return self
-        else: 
-            pass 
-
     def do(self):
-        self.build_flux_query = str(self.build_bucket) + str(self.build_time) + str(self.build_filters) + str(self.build_flatten)
-        return self.build_flux_query
+        if self.bucket_name is None:
+            raise Exception("Must provide bucket")
+        if self.start_time is None:
+            raise Exception("Must provide start time")
+        if self.stop_time is None:
+            raise Exception("Must provide stop time")
 
-query = qBuilder()
-query_bucket = query.bucket("my_bucket").do()
-query_range = query.range("-1hr","now()").do()
-# query_do = query.bucket("test").range("-1hr","now()").filters(filters=[("_measurement", ["abc", "def"]), ("key", ["1","2"])]).flatten(flat=True).do()
-print(query_bucket)
-# print(query_do)
-print(query_range)
-q2 = qBuilder()
-print(q2.build_equals(key="key", value="value"))
+        self.build_flux_query = f'bucket_name = "{self.bucket_name}"\n' + f'start_time = "{self.start_time}"\n' + f'stop_time = "{self.stop_time}"\n'+ f'filters = "{self.filter_tuples}"\n' + str(self.build_bucket) + str(self.build_time) + str(self.build_filters) + str(self.build_flatten)
+        return self
+
+q = qBuilder()
+print(str(q.bucket("test").range("-1hr","now()")))
+print(str(q.bucket("test").range("-1hr","now()").filters(filters=[("_measurement", ["abc", "def"]), ("key", ["1","2"])]).do()))
+
