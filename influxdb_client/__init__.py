82ee030ebd5ec2ed87f1455d46188172e11acdad
Robert Hajek <robert.hajek@gmail.com>
2019-12-06 10:18:00 +0100

    prepare for next development iteration

--- influxdb_client/__init__.py
+++ influxdb_client/__init__.py
@@ -329,4 +329,4 @@ from influxdb_client.client.write_api import WriteApi, WriteOptions
 from influxdb_client.client.influxdb_client import InfluxDBClient
 from influxdb_client.client.write.point import Point
 
-__version__ = '1.2.0'
+__version__ = '1.3.0dev'
