b4df6bfdbffc293c45eacc364d12ac471e09888e
Jakub Bednar <jakub.bednar@gmail.com>
2020-01-22 15:37:07 +0100

    chore: add info about support Python 3.8

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Database',
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules',
