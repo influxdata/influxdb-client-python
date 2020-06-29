9b05758a8873d0eb3f499a58fdd14741b4ee9aaf
Jakub Bednar <jakub.bednar@gmail.com>
2020-06-29 14:49:35 +0200

    chore: add tests job without ciso8601

--- scripts/ci-test.sh
+++ scripts/ci-test.sh
@@ -9,7 +9,10 @@ python --version
 pip install -r requirements.txt --user
 pip install -r extra-requirements.txt --user
 pip install -r test-requirements.txt --user
-pip install -r ciso-requirements.txt --user
+if [ "$DISABLE_CISO_8601" = true ] ; then
+  echo "ciso8601 is disabled"
+  pip install -r ciso-requirements.txt --user
+fi
 pip install pytest pytest-cov --user
 pip install twine --user
 python setup.py sdist bdist_wheel
