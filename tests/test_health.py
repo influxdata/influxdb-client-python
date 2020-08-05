967915a41999772b05c4115fe4f559198bda744d
Jakub Bednar <jakub.bednar@gmail.com>
2020-08-05 13:30:02 +0200

    feat: retry is enabled only for Writes

--- tests/test_health.py
+++ tests/test_health.py
@@ -1,4 +1,8 @@
 import datetime
+import unittest
+
+import httpretty
+import influxdb_client
 
 from influxdb_client import InfluxDBClient
 from tests.base_test import BaseTest
@@ -29,3 +33,32 @@ class TestHealth(BaseTest):
         self.assertIsNotNone(ready.started)
         self.assertTrue(datetime.datetime.now(tz=ready.started.tzinfo) > ready.started)
         self.assertIsNotNone(ready.up)
+
+
+class TestHealthMock(unittest.TestCase):
+
+    def setUp(self) -> None:
+        httpretty.enable()
+        httpretty.reset()
+
+        conf = influxdb_client.configuration.Configuration()
+        conf.host = "http://localhost"
+        conf.debug = False
+
+        self.influxdb_client = InfluxDBClient(url=conf.host, token="my-token")
+
+    def tearDown(self) -> None:
+        self.influxdb_client.__del__()
+        httpretty.disable()
+
+    def test_without_retry(self):
+        httpretty.register_uri(httpretty.GET, uri="http://localhost/health", status=429,
+                               adding_headers={'Retry-After': '5', 'Content-Type': 'application/json'},
+                               body="{\"message\":\"Health is not working\"}")
+
+        check = self.influxdb_client.health()
+        self.assertTrue("Health is not working" in check.message, msg=check.message)
+        self.assertEqual(check.status, "fail")
+        self.assertEqual(check.name, "influxdb")
+
+        self.assertEqual(1, len(httpretty.httpretty.latest_requests))
