f8d1407a6190533faed4d645f6ba973b4d2bc8b2
Jakub Bednar <jakub.bednar@gmail.com>
2020-10-02 07:39:00 +0200

    chore(release): prepare for next development iteration

--- influxdb_client/api_client.py
+++ influxdb_client/api_client.py
@@ -76,7 +76,7 @@ class ApiClient(object):
             self.default_headers[header_name] = header_value
         self.cookie = cookie
         # Set default User-Agent.
-        self.user_agent = 'influxdb-client-python/1.11.0'
+        self.user_agent = 'influxdb-client-python/1.11.0dev'
 
     def __del__(self):
         """Dispose pools."""
