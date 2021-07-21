86f17af36ce2535f0980c5521092ec2b568ef7d9
Jakub Bednar <jakub.bednar@gmail.com>
2021-07-21 08:16:31 +0200

    fix: `DashboardService` responses types

---
+++ tests/test_Dashboards.py
@@ -0,0 +1,33 @@
+import datetime
+
+from influxdb_client import DashboardsService, CreateDashboardRequest, CellsService, \
+    CreateCell
+from tests.base_test import BaseTest
+
+
+class DashboardsClientTest(BaseTest):
+
+    def setUp(self) -> None:
+        super(DashboardsClientTest, self).setUp()
+
+        self.dashboards_service = DashboardsService(self.client.api_client)
+        dashboards = self.dashboards_service.get_dashboards()
+
+        for dashboard in dashboards.dashboards:
+            if dashboard.name.endswith("_IT"):
+                print("Delete dashboard: ", dashboard.name)
+                self.dashboards_service.delete_dashboards_id(dashboard.id)
+
+    def test_create_dashboard_with_cell(self):
+        unique_id = str(datetime.datetime.now().timestamp())
+
+        dashboard = self.dashboards_service.post_dashboards(
+            create_dashboard_request=CreateDashboardRequest(org_id=self.find_my_org().id, name=f"Dashboard_{unique_id}_IT"))
+        self.assertEqual(dashboard.name, f"Dashboard_{unique_id}_IT")
+
+        cells_service = CellsService(self.client.api_client)
+        cell = cells_service.post_dashboards_id_cells(
+            dashboard_id=dashboard.id, create_cell=CreateCell(name=f"Cell_{unique_id}_IT", h=3, w=12))
+        self.assertIsNotNone(cell.id)
+        view = cells_service.get_dashboards_id_cells_id_view(dashboard_id=dashboard.id, cell_id=cell.id)
+        self.assertEqual(view.name, f"Cell_{unique_id}_IT")
