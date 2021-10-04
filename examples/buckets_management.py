6fed0893ad38b10c77f9854ba23a789d43587683
Jakub Bednar <jakub.bednar@gmail.com>
2021-10-04 10:33:15 +0200

    docs: how to write Dictionary-style object

--- examples/buckets_management.py
+++ examples/buckets_management.py
@@ -4,4 +4,21 @@ from influxdb_client.client.write_api import SYNCHRONOUS
 with InfluxDBClient(url='http://localhost:8086', token='my-token', org='my-org') as client:
     write_api = client.write_api(write_options=SYNCHRONOUS)
 
-    write_api.write(bucket='my-bucket', record='h2o_feet,location=coyote_creek water_level=1.0 1')
+    record = [
+        {
+            "measurement": "cpu_load_short",
+            "tags": {
+                "host": "server01",
+                "region": "us-west"
+            },
+            "time": "2009-11-10T23:00:00Z",
+            "fields": {
+                "Float_value": 0.64,
+                "Int_value": 3,
+                "String_value": "Text",
+                "Bool_value": True
+            }
+        }
+    ]
+
+    write_api.write(bucket='my-bucket', record=record)
