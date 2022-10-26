5799519de6f7c1cd95b403f176605c36ead67c81
Jakub Bednar <jakub.bednar@gmail.com>
2022-10-26 17:03:01 +0200

    chore: add supports for Python 3.11

--- setup.py
+++ setup.py
@@ -80,6 +80,7 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Topic :: Database',
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules',
