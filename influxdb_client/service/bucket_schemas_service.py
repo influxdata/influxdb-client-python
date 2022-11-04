193a4597405f4eec8f09759e04ff1111dd229d52
Jakub Bednar <jakub.bednar@gmail.com>
2022-11-04 13:51:01 +0100

    feat: add Explicit bucket schemas API

--- influxdb_client/service/bucket_schemas_service.py
+++ influxdb_client/service/bucket_schemas_service.py
@@ -35,51 +35,51 @@ class BucketSchemasService(_BaseService):
         CloudOnlyWarning.print_warning('BucketSchemasService',
                                        'https://docs.influxdata.com/influxdb/cloud/organizations/buckets/bucket-schema/')  # noqa: E501
 
-    def create_measurement_schema(self, bucket_id, **kwargs):  # noqa: E501,D401,D403
+    def create_measurement_schema(self, bucket_id, measurement_schema_create_request, **kwargs):  # noqa: E501,D401,D403
         """Create a measurement schema for a bucket.
 
         Creates an _explict_ measurement [schema](https://docs.influxdata.com/influxdb/cloud/reference/glossary/#schema) for a bucket.  _Explicit_ schemas are used to enforce column names, tags, fields, and data types for your data.  By default, buckets have an _implicit_ schema-type (`"schemaType": "implicit"`) that conforms to your data.  Use this endpoint to create schemas that prevent non-conforming write requests.  #### Limitations  - Buckets must be created with the "explict" `schemaType` in order to use schemas.  #### Related guides  - [Manage bucket schemas](https://docs.influxdata.com/influxdb/cloud/organizations/buckets/bucket-schema/). - [Create a bucket with an explicit schema](https://docs.influxdata.com/influxdb/cloud/organizations/buckets/create-bucket/#create-a-bucket-with-an-explicit-schema)
         This method makes a synchronous HTTP request by default. To make an
         asynchronous HTTP request, please pass async_req=True
-        >>> thread = api.create_measurement_schema(bucket_id, async_req=True)
+        >>> thread = api.create_measurement_schema(bucket_id, measurement_schema_create_request, async_req=True)
         >>> result = thread.get()
 
         :param async_req bool
         :param str bucket_id: A bucket ID. Adds a schema for the specified bucket. (required)
+        :param MeasurementSchemaCreateRequest measurement_schema_create_request: (required)
         :param str org: An organization name. Specifies the organization that owns the schema.
         :param str org_id: An organization ID. Specifies the organization that owns the schema.
-        :param MeasurementSchemaCreateRequest measurement_schema_create_request:
         :return: MeasurementSchema
                  If the method is called asynchronously,
                  returns the request thread.
         """  # noqa: E501
         kwargs['_return_http_data_only'] = True
         if kwargs.get('async_req'):
-            return self.create_measurement_schema_with_http_info(bucket_id, **kwargs)  # noqa: E501
+            return self.create_measurement_schema_with_http_info(bucket_id, measurement_schema_create_request, **kwargs)  # noqa: E501
         else:
-            (data) = self.create_measurement_schema_with_http_info(bucket_id, **kwargs)  # noqa: E501
+            (data) = self.create_measurement_schema_with_http_info(bucket_id, measurement_schema_create_request, **kwargs)  # noqa: E501
             return data
 
-    def create_measurement_schema_with_http_info(self, bucket_id, **kwargs):  # noqa: E501,D401,D403
+    def create_measurement_schema_with_http_info(self, bucket_id, measurement_schema_create_request, **kwargs):  # noqa: E501,D401,D403
         """Create a measurement schema for a bucket.
 
         Creates an _explict_ measurement [schema](https://docs.influxdata.com/influxdb/cloud/reference/glossary/#schema) for a bucket.  _Explicit_ schemas are used to enforce column names, tags, fields, and data types for your data.  By default, buckets have an _implicit_ schema-type (`"schemaType": "implicit"`) that conforms to your data.  Use this endpoint to create schemas that prevent non-conforming write requests.  #### Limitations  - Buckets must be created with the "explict" `schemaType` in order to use schemas.  #### Related guides  - [Manage bucket schemas](https://docs.influxdata.com/influxdb/cloud/organizations/buckets/bucket-schema/). - [Create a bucket with an explicit schema](https://docs.influxdata.com/influxdb/cloud/organizations/buckets/create-bucket/#create-a-bucket-with-an-explicit-schema)
         This method makes a synchronous HTTP request by default. To make an
         asynchronous HTTP request, please pass async_req=True
-        >>> thread = api.create_measurement_schema_with_http_info(bucket_id, async_req=True)
+        >>> thread = api.create_measurement_schema_with_http_info(bucket_id, measurement_schema_create_request, async_req=True)
         >>> result = thread.get()
 
         :param async_req bool
         :param str bucket_id: A bucket ID. Adds a schema for the specified bucket. (required)
+        :param MeasurementSchemaCreateRequest measurement_schema_create_request: (required)
         :param str org: An organization name. Specifies the organization that owns the schema.
         :param str org_id: An organization ID. Specifies the organization that owns the schema.
-        :param MeasurementSchemaCreateRequest measurement_schema_create_request:
         :return: MeasurementSchema
                  If the method is called asynchronously,
                  returns the request thread.
         """  # noqa: E501
         local_var_params, path_params, query_params, header_params, body_params = \
-            self._create_measurement_schema_prepare(bucket_id, **kwargs)
+            self._create_measurement_schema_prepare(bucket_id, measurement_schema_create_request, **kwargs)
 
         return self.api_client.call_api(
             '/api/v2/buckets/{bucketID}/schema/measurements', 'POST',
@@ -98,7 +98,7 @@ class BucketSchemasService(_BaseService):
             collection_formats={},
             urlopen_kw=kwargs.get('urlopen_kw', None))
 
-    async def create_measurement_schema_async(self, bucket_id, **kwargs):  # noqa: E501,D401,D403
+    async def create_measurement_schema_async(self, bucket_id, measurement_schema_create_request, **kwargs):  # noqa: E501,D401,D403
         """Create a measurement schema for a bucket.
 
         Creates an _explict_ measurement [schema](https://docs.influxdata.com/influxdb/cloud/reference/glossary/#schema) for a bucket.  _Explicit_ schemas are used to enforce column names, tags, fields, and data types for your data.  By default, buckets have an _implicit_ schema-type (`"schemaType": "implicit"`) that conforms to your data.  Use this endpoint to create schemas that prevent non-conforming write requests.  #### Limitations  - Buckets must be created with the "explict" `schemaType` in order to use schemas.  #### Related guides  - [Manage bucket schemas](https://docs.influxdata.com/influxdb/cloud/organizations/buckets/bucket-schema/). - [Create a bucket with an explicit schema](https://docs.influxdata.com/influxdb/cloud/organizations/buckets/create-bucket/#create-a-bucket-with-an-explicit-schema)
@@ -106,15 +106,15 @@ class BucketSchemasService(_BaseService):
 
         :param async_req bool
         :param str bucket_id: A bucket ID. Adds a schema for the specified bucket. (required)
+        :param MeasurementSchemaCreateRequest measurement_schema_create_request: (required)
         :param str org: An organization name. Specifies the organization that owns the schema.
         :param str org_id: An organization ID. Specifies the organization that owns the schema.
-        :param MeasurementSchemaCreateRequest measurement_schema_create_request:
         :return: MeasurementSchema
                  If the method is called asynchronously,
                  returns the request thread.
         """  # noqa: E501
         local_var_params, path_params, query_params, header_params, body_params = \
-            self._create_measurement_schema_prepare(bucket_id, **kwargs)
+            self._create_measurement_schema_prepare(bucket_id, measurement_schema_create_request, **kwargs)
 
         return await self.api_client.call_api(
             '/api/v2/buckets/{bucketID}/schema/measurements', 'POST',
@@ -133,15 +133,19 @@ class BucketSchemasService(_BaseService):
             collection_formats={},
             urlopen_kw=kwargs.get('urlopen_kw', None))
 
-    def _create_measurement_schema_prepare(self, bucket_id, **kwargs):  # noqa: E501,D401,D403
+    def _create_measurement_schema_prepare(self, bucket_id, measurement_schema_create_request, **kwargs):  # noqa: E501,D401,D403
         local_var_params = locals()
 
-        all_params = ['bucket_id', 'org', 'org_id', 'measurement_schema_create_request']  # noqa: E501
+        all_params = ['bucket_id', 'measurement_schema_create_request', 'org', 'org_id']  # noqa: E501
         self._check_operation_params('create_measurement_schema', all_params, local_var_params)
         # verify the required parameter 'bucket_id' is set
         if ('bucket_id' not in local_var_params or
                 local_var_params['bucket_id'] is None):
             raise ValueError("Missing the required parameter `bucket_id` when calling `create_measurement_schema`")  # noqa: E501
+        # verify the required parameter 'measurement_schema_create_request' is set
+        if ('measurement_schema_create_request' not in local_var_params or
+                local_var_params['measurement_schema_create_request'] is None):
+            raise ValueError("Missing the required parameter `measurement_schema_create_request` when calling `create_measurement_schema`")  # noqa: E501
 
         path_params = {}
         if 'bucket_id' in local_var_params:
@@ -430,53 +434,53 @@ class BucketSchemasService(_BaseService):
 
         return local_var_params, path_params, query_params, header_params, body_params
 
-    def update_measurement_schema(self, bucket_id, measurement_id, **kwargs):  # noqa: E501,D401,D403
+    def update_measurement_schema(self, bucket_id, measurement_id, measurement_schema_update_request, **kwargs):  # noqa: E501,D401,D403
         """Update a measurement schema.
 
         Updates a measurement [schema](https://docs.influxdata.com/influxdb/cloud/reference/glossary/#schema).  Use this endpoint to update the fields (`name`, `type`, and `dataType`) of a measurement schema.  #### Limitations  - You can't update the `name` of a measurement.  #### Related guides  - [Manage bucket schemas](https://docs.influxdata.com/influxdb/cloud/organizations/buckets/bucket-schema/). - [Using bucket schemas](https://www.influxdata.com/blog/new-bucket-schema-option-protect-from-unwanted-schema-changes/).
         This method makes a synchronous HTTP request by default. To make an
         asynchronous HTTP request, please pass async_req=True
-        >>> thread = api.update_measurement_schema(bucket_id, measurement_id, async_req=True)
+        >>> thread = api.update_measurement_schema(bucket_id, measurement_id, measurement_schema_update_request, async_req=True)
         >>> result = thread.get()
 
         :param async_req bool
         :param str bucket_id: A bucket ID. Specifies the bucket to retrieve schemas for. (required)
         :param str measurement_id: A measurement schema ID. Retrieves the specified measurement schema. (required)
+        :param MeasurementSchemaUpdateRequest measurement_schema_update_request: (required)
         :param str org: An organization name. Specifies the organization that owns the schema.
         :param str org_id: An organization ID. Specifies the organization that owns the schema.
-        :param MeasurementSchemaUpdateRequest measurement_schema_update_request:
         :return: MeasurementSchema
                  If the method is called asynchronously,
                  returns the request thread.
         """  # noqa: E501
         kwargs['_return_http_data_only'] = True
         if kwargs.get('async_req'):
-            return self.update_measurement_schema_with_http_info(bucket_id, measurement_id, **kwargs)  # noqa: E501
+            return self.update_measurement_schema_with_http_info(bucket_id, measurement_id, measurement_schema_update_request, **kwargs)  # noqa: E501
         else:
-            (data) = self.update_measurement_schema_with_http_info(bucket_id, measurement_id, **kwargs)  # noqa: E501
+            (data) = self.update_measurement_schema_with_http_info(bucket_id, measurement_id, measurement_schema_update_request, **kwargs)  # noqa: E501
             return data
 
-    def update_measurement_schema_with_http_info(self, bucket_id, measurement_id, **kwargs):  # noqa: E501,D401,D403
+    def update_measurement_schema_with_http_info(self, bucket_id, measurement_id, measurement_schema_update_request, **kwargs):  # noqa: E501,D401,D403
         """Update a measurement schema.
 
         Updates a measurement [schema](https://docs.influxdata.com/influxdb/cloud/reference/glossary/#schema).  Use this endpoint to update the fields (`name`, `type`, and `dataType`) of a measurement schema.  #### Limitations  - You can't update the `name` of a measurement.  #### Related guides  - [Manage bucket schemas](https://docs.influxdata.com/influxdb/cloud/organizations/buckets/bucket-schema/). - [Using bucket schemas](https://www.influxdata.com/blog/new-bucket-schema-option-protect-from-unwanted-schema-changes/).
         This method makes a synchronous HTTP request by default. To make an
         asynchronous HTTP request, please pass async_req=True
-        >>> thread = api.update_measurement_schema_with_http_info(bucket_id, measurement_id, async_req=True)
+        >>> thread = api.update_measurement_schema_with_http_info(bucket_id, measurement_id, measurement_schema_update_request, async_req=True)
         >>> result = thread.get()
 
         :param async_req bool
         :param str bucket_id: A bucket ID. Specifies the bucket to retrieve schemas for. (required)
         :param str measurement_id: A measurement schema ID. Retrieves the specified measurement schema. (required)
+        :param MeasurementSchemaUpdateRequest measurement_schema_update_request: (required)
         :param str org: An organization name. Specifies the organization that owns the schema.
         :param str org_id: An organization ID. Specifies the organization that owns the schema.
-        :param MeasurementSchemaUpdateRequest measurement_schema_update_request:
         :return: MeasurementSchema
                  If the method is called asynchronously,
                  returns the request thread.
         """  # noqa: E501
         local_var_params, path_params, query_params, header_params, body_params = \
-            self._update_measurement_schema_prepare(bucket_id, measurement_id, **kwargs)
+            self._update_measurement_schema_prepare(bucket_id, measurement_id, measurement_schema_update_request, **kwargs)
 
         return self.api_client.call_api(
             '/api/v2/buckets/{bucketID}/schema/measurements/{measurementID}', 'PATCH',
@@ -495,7 +499,7 @@ class BucketSchemasService(_BaseService):
             collection_formats={},
             urlopen_kw=kwargs.get('urlopen_kw', None))
 
-    async def update_measurement_schema_async(self, bucket_id, measurement_id, **kwargs):  # noqa: E501,D401,D403
+    async def update_measurement_schema_async(self, bucket_id, measurement_id, measurement_schema_update_request, **kwargs):  # noqa: E501,D401,D403
         """Update a measurement schema.
 
         Updates a measurement [schema](https://docs.influxdata.com/influxdb/cloud/reference/glossary/#schema).  Use this endpoint to update the fields (`name`, `type`, and `dataType`) of a measurement schema.  #### Limitations  - You can't update the `name` of a measurement.  #### Related guides  - [Manage bucket schemas](https://docs.influxdata.com/influxdb/cloud/organizations/buckets/bucket-schema/). - [Using bucket schemas](https://www.influxdata.com/blog/new-bucket-schema-option-protect-from-unwanted-schema-changes/).
@@ -504,15 +508,15 @@ class BucketSchemasService(_BaseService):
         :param async_req bool
         :param str bucket_id: A bucket ID. Specifies the bucket to retrieve schemas for. (required)
         :param str measurement_id: A measurement schema ID. Retrieves the specified measurement schema. (required)
+        :param MeasurementSchemaUpdateRequest measurement_schema_update_request: (required)
         :param str org: An organization name. Specifies the organization that owns the schema.
         :param str org_id: An organization ID. Specifies the organization that owns the schema.
-        :param MeasurementSchemaUpdateRequest measurement_schema_update_request:
         :return: MeasurementSchema
                  If the method is called asynchronously,
                  returns the request thread.
         """  # noqa: E501
         local_var_params, path_params, query_params, header_params, body_params = \
-            self._update_measurement_schema_prepare(bucket_id, measurement_id, **kwargs)
+            self._update_measurement_schema_prepare(bucket_id, measurement_id, measurement_schema_update_request, **kwargs)
 
         return await self.api_client.call_api(
             '/api/v2/buckets/{bucketID}/schema/measurements/{measurementID}', 'PATCH',
@@ -531,10 +535,10 @@ class BucketSchemasService(_BaseService):
             collection_formats={},
             urlopen_kw=kwargs.get('urlopen_kw', None))
 
-    def _update_measurement_schema_prepare(self, bucket_id, measurement_id, **kwargs):  # noqa: E501,D401,D403
+    def _update_measurement_schema_prepare(self, bucket_id, measurement_id, measurement_schema_update_request, **kwargs):  # noqa: E501,D401,D403
         local_var_params = locals()
 
-        all_params = ['bucket_id', 'measurement_id', 'org', 'org_id', 'measurement_schema_update_request']  # noqa: E501
+        all_params = ['bucket_id', 'measurement_id', 'measurement_schema_update_request', 'org', 'org_id']  # noqa: E501
         self._check_operation_params('update_measurement_schema', all_params, local_var_params)
         # verify the required parameter 'bucket_id' is set
         if ('bucket_id' not in local_var_params or
@@ -544,6 +548,10 @@ class BucketSchemasService(_BaseService):
         if ('measurement_id' not in local_var_params or
                 local_var_params['measurement_id'] is None):
             raise ValueError("Missing the required parameter `measurement_id` when calling `update_measurement_schema`")  # noqa: E501
+        # verify the required parameter 'measurement_schema_update_request' is set
+        if ('measurement_schema_update_request' not in local_var_params or
+                local_var_params['measurement_schema_update_request'] is None):
+            raise ValueError("Missing the required parameter `measurement_schema_update_request` when calling `update_measurement_schema`")  # noqa: E501
 
         path_params = {}
         if 'bucket_id' in local_var_params:
