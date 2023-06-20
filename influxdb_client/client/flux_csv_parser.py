02ebe827cfc56c6099186ac10f349e5f4717e8da
Jakub Bednar <jakub.bednar@gmail.com>
2023-06-20 11:02:42 +0200

    fix(data_frame): parsing empty value as `numpy.NaN`

--- influxdb_client/client/flux_csv_parser.py
+++ influxdb_client/client/flux_csv_parser.py
@@ -268,10 +268,12 @@ class FluxCsvParser(object):
         return record
 
     def _to_value(self, str_val, column):
-
         if str_val == '' or str_val is None:
             default_value = column.default_value
             if default_value == '' or default_value is None:
+                if self._serialization_mode is FluxSerializationMode.dataFrame:
+                    from ..extras import np
+                    return self._to_value(np.nan, column)
                 return None
             return self._to_value(default_value, column)
 
