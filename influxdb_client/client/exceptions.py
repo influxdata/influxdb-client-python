f67b5f2c1e9a1e3b7a46f6b014f5e9c4516c9f1f
Ales Pour <ales.pour@bonitoo.io>
2024-05-13 12:53:58 +0200

    fix: internal RESTResponse have getheader() method

--- influxdb_client/client/exceptions.py
+++ influxdb_client/client/exceptions.py
@@ -34,7 +34,7 @@ class InfluxDBError(Exception):
 
         # Header
         for header_key in ["X-Platform-Error-Code", "X-Influx-Error", "X-InfluxDb-Error"]:
-            header_value = response.headers.get(header_key)
+            header_value = response.getheader(header_key)
             if header_value is not None:
                 return header_value
 
