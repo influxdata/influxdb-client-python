5ade22b911f7c5b416f724be44c924dcff9568fe
karel rehor <karl.koerner@bonitoo.io>
2024-08-14 10:41:50 +0200

    feat: refactor error_handling example

--- examples/error_handling.py
+++ examples/error_handling.py
@@ -1,37 +1,28 @@
 import asyncio
-import time
-from datetime import datetime, timezone
+from typing import MutableMapping
 
+from influxdb_client import InfluxDBClient
 from influxdb_client.client.exceptions import InfluxDBError
-
+from influxdb_client.client.influxdb_client_async import InfluxDBClientAsync
+from influxdb_client.client.write_api import SYNCHRONOUS
 from influxdb_client.rest import ApiException
 
-from influxdb_client.client.write_api import SYNCHRONOUS
 
-from influxdb_client import WritePrecision, InfluxDBClient, Point
-from influxdb_client.client.influxdb_client_async import InfluxDBClientAsync
-
-
+# To encapsulate functions used in batch writing
 class BatchCB(object):
 
-    @staticmethod
     def success(self, conf: (str, str, str), data: str):
         print(f"Write success: {conf}, data: {data}")
 
-    @staticmethod
     def error(self, conf: (str, str, str), data: str, exception: InfluxDBError):
-        print(f"Write failed: {conf}, data: {data}, error: {exception.message}")
-        print(f"   Date:                  {exception.headers.get("Date")}")
-        print(f"   X-Influxdb-Build:      {exception.headers.get("X-Influxdb-Build")}")
-        print(f"   X-Influxdb-Version:    {exception.headers.get("X-Influxdb-Version")}")
-        print(f"   X-Platform-Error-Code: {exception.headers.get("X-Platform-Error-Code")}")
-        print(f"   Retry-After:           {exception.headers.get("Retry-After")}")
+        print(f"\nBatch -> Write failed: {conf}, data: {data}, error: {exception.message}")
+        report_headers(exception.headers)
 
-    @staticmethod
     def retry(self, conf: (str, str, str), data: str, exception: InfluxDBError):
         print(f"Write failed but retryable: {conf}, data: {data}, error: {exception}")
 
 
+# simple reporter that server is available
 def report_ping(ping: bool):
     if not ping:
         raise ValueError("InfluxDB: Failed to ping server")
@@ -39,6 +30,16 @@ def report_ping(ping: bool):
         print("InfluxDB: ready")
 
 
+# report some useful expected header fields
+def report_headers(headers: MutableMapping[str, str]):
+    print("   Date:                  ", headers.get("Date"))
+    print("   X-Influxdb-Build:      ", headers.get("X-Influxdb-Build"))
+    print("   X-Influxdb-Version:    ", headers.get("X-Influxdb-Version"))
+    print("   X-Platform-Error-Code: ", headers.get("X-Platform-Error-Code"))
+    print("   Retry-After:           ", headers.get("Retry-After"))  # Should be None
+
+
+# try a write using a synchronous call
 def use_sync():
     print("Using sync")
     with InfluxDBClient(url="http://localhost:8086", token="my-token", org="my-org") as client:
@@ -46,16 +47,13 @@ def use_sync():
         try:
             client.write_api(write_options=SYNCHRONOUS).write(bucket="my-bucket", record="cpu,location=G4 usage=")
         except ApiException as ae:
-            print("\nCaught ae: ", ae.message)
-            print("   Date:                  ", ae.headers.get("Date"))
-            print("   X-Influxdb-Build:      ", ae.headers.get("X-Influxdb-Build"))
-            print("   X-Influxdb-Version:    ", ae.headers.get("X-Influxdb-Version"))
-            print("   X-Platform-Error-Code: ", ae.headers.get("X-Platform-Error-Code"))
-            print("   Retry-After:           ", ae.headers.get("Retry-After"))  # Should be None
+            print("\nSync -> Caught ApiException: ", ae.message)
+            report_headers(ae.headers)
 
         print("Sync write done")
 
 
+# try a write using batch API
 def use_batch():
     print("Using batch")
     with InfluxDBClient(url="http://localhost:8086", token="my-token", org="my-org") as client:
@@ -65,8 +63,10 @@ def use_batch():
                               retry_callback=cb.retry) as write_api:
             write_api.write(bucket="my-bucket", record="cpu,location=G9 usage=")
             print("Batch write sent")
+        print("Batch write done")
 
 
+# try a write using async.io
 async def use_async():
     print("Using async")
     async with InfluxDBClientAsync(url="http://localhost:8086", token="my-token", org="my-org") as client:
@@ -74,13 +74,14 @@ async def use_async():
         try:
             await client.write_api().write(bucket="my-bucket", record="cpu,location=G7 usage=")
         except InfluxDBError as ie:
-            print("\nCaught ie: ", ie)
+            print("\nAsync -> Caught InfluxDBError: ", ie.message)
+            report_headers(ie.headers)
         print("Async write done")
 
 
 if __name__ == "__main__":
     use_sync()
-    print("\n   Continuing...")
+    print("\n   Continuing...\n")
     use_batch()
-    print("\n   Continuing...")
+    print("\n   Continuing...\n")
     asyncio.run(use_async())
