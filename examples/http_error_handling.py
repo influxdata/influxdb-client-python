db7df2071d9d122b5984d7519922bcd0d20c0666
karel rehor <karl.koerner@bonitoo.io>
2024-08-14 17:09:50 +0200

    chore: refactor example http_error_handling to use cloud and header trace-id

--- examples/http_error_handling.py
+++ examples/http_error_handling.py
@@ -1,4 +1,16 @@
+"""
+Illustrates getting header values from Errors that may occur on write.
+
+To test against cloud set the following environment variables:
+   INFLUX_URL
+   INFLUX_TOKEN
+   INFLUX_DATABASE
+   INFLUX_ORG
+
+...otherwise will run against a standard OSS endpoint.
+"""
 import asyncio
+import os
 from typing import MutableMapping
 
 from influxdb_client import InfluxDBClient
@@ -6,6 +18,30 @@ from influxdb_client.client.exceptions import InfluxDBError
 from influxdb_client.client.influxdb_client_async import InfluxDBClientAsync
 from influxdb_client.client.write_api import SYNCHRONOUS
 from influxdb_client.rest import ApiException
+
+
+def get_envar(key, default):
+    try:
+        return os.environ[key]
+    except:
+        return default
+
+
+class Config(object):
+
+    def __init__(self):
+        self.url = get_envar("INFLUX_URL", "http://localhost:8086")
+        self.token = get_envar("INFLUX_TOKEN", "my-token")
+        self.bucket = get_envar("INFLUX_DATABASE", "my-bucket")
+        self.org = get_envar("INFLUX_ORG", "my-org")
+
+    def __str__(self):
+        return (f"config:\n"
+                f"   url: {self.url}\n"
+                f"   token: ****redacted*****\n"
+                f"   bucket: {self.bucket}\n"
+                f"   org: {self.org}\n"
+                )
 
 
 # To encapsulate functions used in batch writing
@@ -34,18 +70,19 @@ def report_ping(ping: bool):
 def report_headers(headers: MutableMapping[str, str]):
     print("   Date:                  ", headers.get("Date"))
     print("   X-Influxdb-Build:      ", headers.get("X-Influxdb-Build"))
-    print("   X-Influxdb-Version:    ", headers.get("X-Influxdb-Version"))
-    print("   X-Platform-Error-Code: ", headers.get("X-Platform-Error-Code"))
+    print("   X-Influxdb-Version:    ", headers.get("X-Influxdb-Version")) # OSS version, Cloud should be None
+    print("   X-Platform-Error-Code: ", headers.get("X-Platform-Error-Code")) # OSS invalid, Cloud should be None
     print("   Retry-After:           ", headers.get("Retry-After"))  # Should be None
+    print("   Trace-Id:              ", headers.get("Trace-Id")) # OSS should be None, Cloud should return value
 
 
 # try a write using a synchronous call
-def use_sync():
+def use_sync(conf: Config):
     print("Using sync")
-    with InfluxDBClient(url="http://localhost:8086", token="my-token", org="my-org") as client:
+    with InfluxDBClient(url=conf.url, token=conf.token, org=conf.org) as client:
         report_ping(client.ping())
         try:
-            client.write_api(write_options=SYNCHRONOUS).write(bucket="my-bucket", record="cpu,location=G4 usage=")
+            client.write_api(write_options=SYNCHRONOUS).write(bucket=conf.bucket, record="cpu,location=G4 usage=")
         except ApiException as ae:
             print("\nSync -> Caught ApiException: ", ae.message)
             report_headers(ae.headers)
@@ -54,25 +91,25 @@ def use_sync():
 
 
 # try a write using batch API
-def use_batch():
+def use_batch(conf: Config):
     print("Using batch")
-    with InfluxDBClient(url="http://localhost:8086", token="my-token", org="my-org") as client:
+    with InfluxDBClient(url=conf.url, token=conf.token, org=conf.org) as client:
         cb = BatchCB()
         with client.write_api(success_callback=cb.success,
                               error_callback=cb.error,
                               retry_callback=cb.retry) as write_api:
-            write_api.write(bucket="my-bucket", record="cpu,location=G9 usage=")
+            write_api.write(bucket=conf.bucket, record="cpu,location=G9 usage=")
             print("Batch write sent")
         print("Batch write done")
 
 
 # try a write using async.io
-async def use_async():
+async def use_async(conf: Config):
     print("Using async")
-    async with InfluxDBClientAsync(url="http://localhost:8086", token="my-token", org="my-org") as client:
+    async with InfluxDBClientAsync(url=conf.url, token=conf.token, org=conf.org) as client:
         report_ping(await client.ping())
         try:
-            await client.write_api().write(bucket="my-bucket", record="cpu,location=G7 usage=")
+            await client.write_api().write(bucket=conf.bucket, record="cpu,location=G7 usage=")
         except InfluxDBError as ie:
             print("\nAsync -> Caught InfluxDBError: ", ie.message)
             report_headers(ie.headers)
@@ -80,8 +117,10 @@ async def use_async():
 
 
 if __name__ == "__main__":
-    use_sync()
+    conf = Config()
+    print(conf)
+    use_sync(conf)
     print("\n   Continuing...\n")
-    use_batch()
+    use_batch(conf)
     print("\n   Continuing...\n")
-    asyncio.run(use_async())
+    asyncio.run(use_async(conf))
