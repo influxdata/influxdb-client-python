0b1d66744ee3dcd6176472dc3beae93d09f7bfd2
NguyenHoangSon96 <hoangsonnguyen4321@gmail.com>
2026-04-10 05:47:30 +0700

    Revert "fix: test redirect"
    This reverts commit a869b1a048f21d0b567c2a171040e2537f9a58f6.

--- tests/test_WriteApi.py
+++ tests/test_WriteApi.py
@@ -532,7 +532,7 @@ class WriteApiTestMock(BaseTest):
         Retry.DEFAULT.remove_headers_on_redirect = Retry.DEFAULT_REMOVE_HEADERS_ON_REDIRECT
         self.influxdb_client.close()
 
-        retries = Retry(redirect=1, remove_headers_on_redirect=[])
+        retries = Retry(redirect=2)
         self.influxdb_client = InfluxDBClient(url="http://localhost", token="my-token", org="my-org", retries=retries)
 
         httpretty.register_uri(httpretty.POST, uri="http://localhost2/api/v2/write", status=204)
